ac25b4c6335ad469f16a4f05ff111369dfd15a2d
xADDBx <62178123+xADDBx@users.noreply.github.com>
2026-01-13 03:36:07 +0100

    Entrypoint adjustments

--- BpBinReader/Entry.cs
+++ BpBinReader/Entry.cs
@@ -1,21 +1,28 @@
-﻿namespace BpBinReader;
+﻿using System.Runtime.InteropServices;
+using System.Text.RegularExpressions;
+
+namespace BpBinReader;
 
 public class Entry {
 
     public static void Main(string[] args) {
         if (args.Length < 1) {
-            Console.WriteLine("Usage: BpBinReader <basePath>");
-            Console.WriteLine("  <basePath> The base path to the game installation.");
-
-            return;
+            try {
+                var installLocation = FindInstallLocation();
+                Console.WriteLine($"Found install location: {installLocation}");
+                args = [installLocation];
+            } catch (NotSupportedException) {
+                Console.WriteLine("Usage: BpBinReader <basePath>");
+                Console.WriteLine("  <basePath> The base path to the game installation.");
+                return;
+            }
         }
         var basePath = args[0];
-        // var basePath = "D:\\Games\\Steam\\steamapps\\common\\Warhammer 40,000 Rogue Trader";
         try {
             var schemaProvider = new RogueTraderTypeSchemaProvider([Path.Combine(basePath, "WH40KRT_Data", "Managed")]);
 
             var packPath = Path.Combine(basePath, "Bundles", "blueprints-pack.bbp");
-            var outputPath = Path.Combine(Path.GetDirectoryName(typeof(Entry).Assembly.Location)!, "blueprints.json");
+            var outputPath = Path.Combine(AppContext.BaseDirectory, "blueprints.json");
 
             BinaryToJsonConverter.DumpBlueprintPackToJson(packPath, outputPath, schemaProvider);
 
@@ -25,5 +32,26 @@ public class Entry {
             Console.WriteLine(ex.ToString());
             throw;
         }
+    }
+    private static string FindInstallLocation() {
+        try {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows)) {
+                var rogueTraderDataPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData) + "Low", "Owlcat Games", "Warhammer 40000 Rogue Trader", "Player.log");
+                string lineToFind = "Mono path[0]";
+                string line = null;
+                foreach (var lineIter in File.ReadLines(rogueTraderDataPath)) {
+                    if (lineIter.Contains(lineToFind)) {
+                        line = lineIter;
+                        break;
+                    }
+                }
+                string monoPathRegex = @"^Mono path\[0\] = '(.*?)/WH40KRT_Data/Managed'$";
+                Match match = Regex.Match(line, monoPathRegex);
+                if (match.Success) {
+                    return match.Groups[1].Value;
+                }
+            }
+        } catch { }
+        throw new NotSupportedException("Could not automatically find the install location for this OS platform.");
     }
 }
