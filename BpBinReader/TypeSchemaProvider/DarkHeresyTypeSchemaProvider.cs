e34880add97bc87c5c7373a260d9e99c34039124
xADDBx <62178123+xADDBx@users.noreply.github.com>
2026-01-31 19:20:14 +0100

    Abstract out stuff to better reflect differences between games

--- BpBinReader/TypeSchemaProvider/DarkHeresyTypeSchemaProvider.cs
+++ BpBinReader/TypeSchemaProvider/DarkHeresyTypeSchemaProvider.cs
@@ -1,38 +1,10 @@
-﻿using System.Reflection;
+﻿namespace BpBinReader;
 
-namespace BpBinReader;
-
-public sealed class DarkHeresyTypeSchemaProvider : MetadataLoadContextTypeSchemaProvider {
-    private readonly Type m_TypeIdAttributeType;
-    private readonly Type m_ExcludeFieldFromBuildAttributeType;
-    private readonly Type m_ModsPatchSerializableAttributeType;
-    private readonly Type m_SimpleBlueprintType;
-    private readonly Type m_BlueprintComponentType;
-    private readonly Type m_ElementType;
+public sealed class DarkHeresyTypeSchemaProvider : V1MetadataBase {
     private readonly Type m_SerializeReferenceAttributeType;
-    protected override Type BlueprintReferenceBaseType { get; }
 
-    public DarkHeresyTypeSchemaProvider(IEnumerable<string> assemblyDirectoryPaths) : base(assemblyDirectoryPaths) {
-        m_TypeIdAttributeType = RequireType("Owlcat.Runtime.Core.Utility.TypeIdAttribute");
-        m_ExcludeFieldFromBuildAttributeType = RequireType("Kingmaker.Blueprints.JsonSystem.Helpers.ExcludeFieldFromBuildAttribute");
-        m_ModsPatchSerializableAttributeType = RequireType("Kingmaker.Blueprints.JsonSystem.Helpers.ModsPatchSerializableAttribute");
-        BlueprintReferenceBaseType = RequireType("Kingmaker.Blueprints.BlueprintReferenceBase");
-        m_SimpleBlueprintType = RequireType("Kingmaker.Blueprints.SimpleBlueprint");
-        m_BlueprintComponentType = RequireType("Kingmaker.Blueprints.BlueprintComponent");
-        m_ElementType = RequireType("Kingmaker.ElementsSystem.Element");
+    public DarkHeresyTypeSchemaProvider(IEnumerable<string> assemblyDirectoryPaths) : base(assemblyDirectoryPaths, "Owlcat.Runtime.Core.Utility.TypeIdAttribute") {
         m_SerializeReferenceAttributeType = RequireType("UnityEngine.SerializeReference");
-
-        foreach (var t in TypeByFullName.Values) {
-            GetAttribute(t, m_TypeIdAttributeType, out var attr);
-            if (attr != null) {
-                string? guid = attr.ConstructorArguments[0].Value as string;
-                if (string.IsNullOrWhiteSpace(guid)) {
-                    continue;
-                }
-
-                TypeById[new(guid)] = t;
-            }
-        }
     }
     protected override TypeSchema BuildSchema(Type type, Guid typeId) {
         var fields = GetUnitySerializedFields(type)
@@ -44,84 +16,12 @@ public sealed class DarkHeresyTypeSchemaProvider : MetadataLoadContextTypeSchema
                     candidateType = candidateType.GetGenericArguments()[0];
                 }
 
-                var forceNeedsType = HasAttribute(f, m_SerializeReferenceAttributeType) && candidateType != null && HasAttribute(candidateType, m_TypeIdAttributeType);
+                var forceNeedsType = HasAttribute(f, m_SerializeReferenceAttributeType) && candidateType != null && HasAttribute(candidateType, TypeIdAttributeType);
 
                 return new FieldSchema(f.Name, BuildValueSchema(f.FieldType, forceNeedsType));
             })
             .ToArray();
         // Console.WriteLine($"{type.FullName}: [{string.Join(", ", fields.Select(f => f.Name))}]");
         return new TypeSchema(type.Name, type.FullName ?? type.Name, fields, type, typeId);
-    }
-    protected override IEnumerable<FieldInfo> InternalGetUnitySerializedFields(Type type) {
-        return FieldsContractResolver_GetUnitySerializedFields(type)
-            .Where(f => !HasAttribute(f, JsonIgnoreAttributeType))
-            .Where(f => !HasAttribute(f, m_ExcludeFieldFromBuildAttributeType));
-    }
-    private IEnumerable<FieldInfo> FieldsContractResolver_GetUnitySerializedFields(Type type) {
-        List<FieldInfo> allFields = [];
-        if (type.BaseType != null) {
-            allFields.AddRange(FieldsContractResolver_GetUnitySerializedFields(type.BaseType));
-        }
-        allFields.AddRange(type.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
-            .Where(f => f.IsPublic || HasAttribute(f, SerializeFieldAttributeType))
-            .Where(f => !HasAttribute(f, NonSerializedAttributeType))
-            .Where(f => FieldsContractResolver_IsSerializableType(f, f.FieldType, true)));
-        return allFields;
-    }
-    private bool FieldsContractResolver_IsSerializableType(FieldInfo field, Type? fieldType, bool arraysAllowed) {
-        if (fieldType == null) {
-            return false;
-        }
-        var fn = fieldType.FullName ?? fieldType.Name;
-
-        if (fieldType.IsPrimitive || fieldType.IsEnum || fn == "System.String") {
-            return true;
-        }
-
-        if (IsOrSubclassOf(fieldType, UnityObjectType)) {
-            return true;
-        }
-
-        if (fn == "System.Object" && HasAttribute(field, m_ModsPatchSerializableAttributeType)) {
-            return true;
-        }
-
-        if (fn == "UnityEngine.AnimationCurve"
-            || fn == "UnityEngine.Rect"
-            || fn == "UnityEngine.Vector2"
-            || fn == "UnityEngine.Vector3"
-            || fn == "UnityEngine.Vector4"
-            || fn == "UnityEngine.Vector2Int"
-            || fn == "UnityEngine.Vector3Int"
-            || fn == "UnityEngine.Color"
-            || fn == "UnityEngine.Color32"
-            || fn == "UnityEngine.Gradient") {
-            return true;
-        }
-
-        if (fieldType.IsArray) {
-            if (!arraysAllowed) {
-                return false;
-            }
-            return FieldsContractResolver_IsSerializableType(field, fieldType.GetElementType(), false);
-        }
-
-        if (IsList(fieldType)) {
-            if (!arraysAllowed) {
-                return false;
-            }
-            return FieldsContractResolver_IsSerializableType(field, fieldType.GetGenericArguments()[0], false);
-        }
-
-        if (HasAttribute(fieldType, SerializableAttributeType)) {
-            var asmName = fieldType.Assembly.GetName().Name;
-            return !string.Equals(asmName, "mscorlib", StringComparison.OrdinalIgnoreCase);
-        }
-
-        return false;
-    }
-
-    protected override bool IsIdentifiedType(Type t) {
-        return IsOrSubclassOf(t, m_SimpleBlueprintType) || IsOrSubclassOf(t, m_BlueprintComponentType) || IsOrSubclassOf(t, m_ElementType);
     }
 }
---
+++ BpBinReader/TypeSchemaProvider/V1MetadataBase.cs
@@ -0,0 +1,114 @@
+﻿using System.Reflection;
+
+namespace BpBinReader;
+
+public abstract class V1MetadataBase : MetadataLoadContextTypeSchemaProvider {
+    protected override Type BlueprintReferenceBaseType { get; }
+    protected readonly Type TypeIdAttributeType;
+    protected readonly Type ExcludeFieldFromBuildAttributeType;
+    protected readonly Type ModsPatchSerializableAttributeType;
+    protected readonly Type SimpleBlueprintType;
+    protected readonly Type BlueprintComponentType;
+    protected readonly Type ElementType;
+    protected V1MetadataBase(IEnumerable<string> assemblyDirectoryPaths, string typeIdAttribute) : base(assemblyDirectoryPaths) {
+        TypeIdAttributeType = RequireType(typeIdAttribute);
+        ExcludeFieldFromBuildAttributeType = RequireType("Kingmaker.Blueprints.JsonSystem.Helpers.ExcludeFieldFromBuildAttribute");
+        ModsPatchSerializableAttributeType = RequireType("Kingmaker.Blueprints.JsonSystem.Helpers.ModsPatchSerializableAttribute");
+        SimpleBlueprintType = RequireType("Kingmaker.Blueprints.SimpleBlueprint");
+        BlueprintComponentType = RequireType("Kingmaker.Blueprints.BlueprintComponent");
+        ElementType = RequireType("Kingmaker.ElementsSystem.Element");
+        BlueprintReferenceBaseType = RequireType("Kingmaker.Blueprints.BlueprintReferenceBase");
+
+
+        foreach (var t in TypeByFullName.Values) {
+            GetAttribute(t, TypeIdAttributeType, out var attr);
+            if (attr != null) {
+                string? guid = attr.ConstructorArguments[0].Value as string;
+                if (string.IsNullOrWhiteSpace(guid)) {
+                    continue;
+                }
+
+                TypeById[new(guid)] = t;
+            }
+        }
+    }
+    protected override TypeSchema BuildSchema(Type type, Guid typeId) {
+        var fields = GetUnitySerializedFields(type)
+            .Select(f => new FieldSchema(f.Name, BuildValueSchema(f.FieldType)))
+            .ToArray();
+        // Console.WriteLine($"{type.FullName}: [{string.Join(", ", fields.Select(f => f.Name))}]");
+        return new TypeSchema(type.Name, type.FullName ?? type.Name, fields, type, typeId);
+    }
+    protected override IEnumerable<FieldInfo> InternalGetUnitySerializedFields(Type type) {
+        return FieldsContractResolver_GetUnitySerializedFields(type)
+            .Where(f => !HasAttribute(f, JsonIgnoreAttributeType))
+            .Where(f => !HasAttribute(f, ExcludeFieldFromBuildAttributeType));
+    }
+    private IEnumerable<FieldInfo> FieldsContractResolver_GetUnitySerializedFields(Type type) {
+        List<FieldInfo> allFields = [];
+        if (type.BaseType != null) {
+            allFields.AddRange(FieldsContractResolver_GetUnitySerializedFields(type.BaseType));
+        }
+        allFields.AddRange(type.GetFields(BindingFlags.DeclaredOnly | BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic)
+            .Where(f => f.IsPublic || HasAttribute(f, SerializeFieldAttributeType))
+            .Where(f => !HasAttribute(f, NonSerializedAttributeType))
+            .Where(f => FieldsContractResolver_IsSerializableType(f, f.FieldType, true)));
+        return allFields;
+    }
+    private bool FieldsContractResolver_IsSerializableType(FieldInfo field, Type? fieldType, bool arraysAllowed) {
+        if (fieldType == null) {
+            return false;
+        }
+        var fn = fieldType.FullName ?? fieldType.Name;
+
+        if (fieldType.IsPrimitive || fieldType.IsEnum || fn == "System.String") {
+            return true;
+        }
+
+        if (IsOrSubclassOf(fieldType, UnityObjectType)) {
+            return true;
+        }
+
+        if (fn == "System.Object" && HasAttribute(field, ModsPatchSerializableAttributeType)) {
+            return true;
+        }
+
+        if (fn == "UnityEngine.AnimationCurve"
+            || fn == "UnityEngine.Rect"
+            || fn == "UnityEngine.Vector2"
+            || fn == "UnityEngine.Vector3"
+            || fn == "UnityEngine.Vector4"
+            || fn == "UnityEngine.Vector2Int"
+            || fn == "UnityEngine.Vector3Int"
+            || fn == "UnityEngine.Color"
+            || fn == "UnityEngine.Color32"
+            || fn == "UnityEngine.Gradient") {
+            return true;
+        }
+
+        if (fieldType.IsArray) {
+            if (!arraysAllowed) {
+                return false;
+            }
+            return FieldsContractResolver_IsSerializableType(field, fieldType.GetElementType(), false);
+        }
+
+        if (IsList(fieldType)) {
+            if (!arraysAllowed) {
+                return false;
+            }
+            return FieldsContractResolver_IsSerializableType(field, fieldType.GetGenericArguments()[0], false);
+        }
+
+        if (HasAttribute(fieldType, SerializableAttributeType)) {
+            var asmName = fieldType.Assembly.GetName().Name;
+            return !string.Equals(asmName, "mscorlib", StringComparison.OrdinalIgnoreCase);
+        }
+
+        return false;
+    }
+
+    protected override bool IsIdentifiedType(Type t) {
+        return IsOrSubclassOf(t, SimpleBlueprintType) || IsOrSubclassOf(t, BlueprintComponentType) || IsOrSubclassOf(t, ElementType);
+    }
+}
